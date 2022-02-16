67127e6bb74bd4ff86fc084551e36f3b05c7db1d
Sanju Poojari <36479146+SanjayDev01@users.noreply.github.com>
2022-02-16 08:09:42 +0530

    splash screen done

--- lib/screens/splashscreen.dart
+++ lib/screens/splashscreen.dart
@@ -1,4 +1,9 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:hexcolor/hexcolor.dart';
+import 'package:newsexample/auth/signin_screen.dart';
+import 'package:shimmer/shimmer.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({Key? key}) : super(key: key);
@@ -9,7 +14,63 @@ class SplashScreen extends StatefulWidget {
 
 class _SplashScreenState extends State<SplashScreen> {
   @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    // startTimeout();
+  }
+
+  handleTimeout() {
+    Navigator.pushReplacement(
+        context, MaterialPageRoute(builder: (context) => const SignInScreen()));
+  }
+
+  startTimeout() {
+    var duration = const Duration(seconds: 2);
+    return Timer(duration, handleTimeout);
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(colors: [
+            HexColor("#203F3B"),
+            HexColor("#3F3F3F"),
+          ], begin: Alignment.topCenter, end: Alignment.bottomCenter),
+        ),
+        child: Center(
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(
+              width: 200.0,
+              // height: 100.0,
+              child: Shimmer.fromColors(
+                baseColor: Colors.redAccent,
+                highlightColor: Colors.white38,
+                child: const Text(
+                  'News App',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 40.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 5.0,
+            ),
+            const Text("using newsapi.org",
+                style: TextStyle(
+                    fontSize: 18.0,
+                    fontWeight: FontWeight.normal,
+                    color: Colors.white30)),
+          ],
+        )),
+      ),
+    );
   }
 }
